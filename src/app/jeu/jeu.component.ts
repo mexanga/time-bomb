a7e86788acdb1935e87dc66691f9b801a6b5071a
Ulysse ARNAUD <mexanga@gmail.com>
2020-06-24 23:33:17 +0200

    MAJ de JeuComponent: possibilité de choisir le nombre de joueur

--- src/app/jeu/jeu.component.ts
+++ src/app/jeu/jeu.component.ts
@@ -11,9 +11,23 @@ export class JeuComponent implements OnInit {
   public jeu: Jeu;
   public jeuLance: boolean;
 
-  constructor() { }
+  public nombreJoueurs: number;
+  public nombreJoueursMin = Jeu.NombreJoueurs.min;
+  public nombreJoueursMax = Jeu.NombreJoueurs.max;
+
+  constructor() {
+    this.nombreJoueurs = this.nombreJoueursMin;
+  }
 
   ngOnInit(): void {
+  }
+
+  decrementerNombreJoueurs(): void {
+    this.nombreJoueurs = Math.max(this.nombreJoueursMin, this.nombreJoueurs - 1);
+  }
+
+  incrementerNombreJoueurs(): void {
+    this.nombreJoueurs = Math.min(this.nombreJoueursMax, this.nombreJoueurs + 1);
   }
 
 }
