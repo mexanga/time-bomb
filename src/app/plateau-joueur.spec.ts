4f1e48635140f3a72229b52e1c6c913ef02e0e07
Ulysse ARNAUD <mexanga@gmail.com>
2020-06-24 14:23:08 +0200

    Ajout des plateaux de jeu pour un joueur : il manquait la classe dans le precedent commit

---
+++ src/app/plateau-joueur.spec.ts
@@ -0,0 +1,7 @@
+import { PlateauJoueur } from './plateau-joueur';
+
+describe('PlateauJoueur', () => {
+  it('should create an instance', () => {
+    expect(new PlateauJoueur()).toBeTruthy();
+  });
+});
