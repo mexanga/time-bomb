6d2b7bc21ab799342bbb3c75e5cb8eaeddec0151
Ulysse ARNAUD <mexanga@gmail.com>
2020-06-24 16:48:29 +0200

    Ajout des decks (classe)

---
+++ src/app/deck.ts
@@ -0,0 +1,26 @@
+import {Carte} from './carte';
+
+export class Deck {
+
+  private cartes: Carte[];
+
+  constructor(
+    cartes: Carte[]
+  ) {
+    this.cartes = cartes;
+  }
+
+
+  public melanger() {
+    // TODO
+  }
+
+  public enlever(carte: Carte) {
+    // TODO
+  }
+
+  public initialiser () {
+    // TODO
+  }
+
+}
