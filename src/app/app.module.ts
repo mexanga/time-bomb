efb5f66027303a88562c5b39dbf7751baea14b61
Ulysse ARNAUD <mexanga@gmail.com>
2020-06-24 14:23:26 +0200

    MAJ de AppModule : Ajout des cartes et plateaux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlateauJoueurComponent } from './plateau-joueur/plateau-joueur.component';
+import { CarteComponent } from './carte/carte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlateauJoueurComponent,
+    CarteComponent
   ],
   imports: [
     BrowserModule,
